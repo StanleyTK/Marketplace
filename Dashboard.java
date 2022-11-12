e72f8a8abebf94613f497907c5852af8f1142b2f
RichardSCodes <su304@purdue.edu>
2022-11-12 02:19:12 -0500

    import/export csv for sellers

--- Dashboard.java
+++ Dashboard.java
@@ -13,29 +13,91 @@ public class Dashboard {
     }
 
     // export csv
-    public static void csvFile() {
         //TODO Sellers can import or export products for their stores using a csv file.
         //TODO All product details should be included, with one row per product.
-
-    }
     // import csv
-//    public static void csvFile(Seller seller) {
-//        try {
-//            File f = new File("f");
-//            FileReader fr = new FileReader(f);
-//            BufferedReader bfr = new BufferedReader(fr);
-//            String line = bfr.readLine();
-//            ArrayList<Product> products = new ArrayList<Product>();
-//            while (line != null) {
-//                Product product = MarketPlace.getProduct(line);
-//                products.add(product);
-//                seller.setProducts(products);
-//                line = bfr.readLine();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void csvFile() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        while(true) {
+            try {
+                System.out.println("Would you like to import or export?\n1. Import\n2. Export");
+                option = Integer.parseInt(scanner.nextLine());
+                if (option == 1 || option == 2) {
+                    break;
+                }
+                System.out.println("Enter a valid input!");
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid input!");
+            }
+        }
+        System.out.println("Which market would you like to import or export products to?");
+        String storeName = scanner.nextLine();
+        File markets = new File("Markets.txt");
+        BufferedReader bfr = new BufferedReader(new FileReader(markets));
+        String line;
+        boolean bol = false;
+        File f = null;
+        while ((line = bfr.readLine()) != null) {
+            if (storeName.equals(line)) {
+                f = new File(storeName + " Market.txt");
+                bol = true;
+            }
+        }
+        bfr.close();
+        if (bol) {
+            switch (option) {
+                case (1) -> {
+                    System.out.println("What is the file location you are importing from?");
+                    File file = new File(scanner.nextLine());
+                    FileReader fr = new FileReader(file);
+                    bfr = new BufferedReader(fr);
+                    ArrayList<Product> products = new ArrayList<Product>();
+                    line = bfr.readLine();
+                    while (line != null) {
+                        products.add(MarketPlace.getProduct(line));
+                        line = bfr.readLine();
+                    }
+                    bfr.close();
+                    bfr = new BufferedReader(new FileReader(f));
+                    line = bfr.readLine();
+                    while (!line.equals("--------")) {
+                        line = bfr.readLine();
+                    }
+                    String rest = "";
+                    while (line != null) {
+                        rest += line + "\n";
+                        line = bfr.readLine();
+                    }
+                    FileOutputStream fos = new FileOutputStream(f);
+                    PrintWriter pw = new PrintWriter(fos);
+                    for (int i = 0; i < products.size(); i++) {
+                        pw.println(products.get(i).toString());
+                    }
+                    pw.print(rest);
+                    pw.close();
+                    bfr.close();
+                    break;
+                }
+                case (2) -> {
+                    System.out.println("What is the file location where the products should be exported?");
+                    File file = new File(scanner.nextLine());
+                    bfr = new BufferedReader(new FileReader(f));
+                    line = bfr.readLine();
+                    String products = "";
+                    while (!line.equals("--------")) {
+                        products += line + "\n";
+                        line = bfr.readLine();
+                    }
+                    PrintWriter pw = new PrintWriter(new FileOutputStream(file));
+                    pw.print(products);
+                    pw.close();
+                    bfr.close();
+                    break;
+                }
+            }
+        }
+    }
 
     public static void exportPurchaseHistory() {
         //TODO Customers can export a file with their purchase history.
