0981e788c85ca805934adf78cba62034647aaa10
Stanley Kim <kim4111@purdue.edu>
2022-11-13 18:30:19 -0500

    updated

--- MarketPlace.java
+++ MarketPlace.java
@@ -81,9 +81,9 @@ public class MarketPlace {
                         case (2) -> CustomerOptions.searchForProducts(scanner);
                         case (3) -> CustomerOptions.sortByPrice();
                         case (4) -> CustomerOptions.sortByQuantity();
-                        case (5) -> Dashboard.viewCustomer(); //TODO
+                        case (5) -> Dashboard.viewCustomer();
                         case (6) -> Dashboard.exportPurchaseHistory((Customer) user);
-                        case (7) -> CustomerOptions.addOrRemoveProductsShoppingCart(scanner, user.getCustomerName()); //TODO
+                        case (7) -> CustomerOptions.addOrRemoveProductsShoppingCart(scanner, user.getCustomerName());
                         case (8) -> {
                             running = false;
                         }
@@ -111,10 +111,10 @@ public class MarketPlace {
                     switch (option) {
                         case (1) -> CustomerOptions.viewMarket();
                         case (2) -> SellerOptions.editProducts(scanner);
-                        case (3) -> SellerOptions.viewSales();//TODO
-                        case (4) -> Dashboard.viewSeller(); //TODO
+                        case (3) -> SellerOptions.viewSales();
+                        case (4) -> Dashboard.viewSeller();
                         case (5) -> Dashboard.csvFile();
-                        case (6) -> SellerOptions.viewCustomerShoppingCarts(); //TODO
+                        case (6) -> SellerOptions.viewCustomerShoppingCarts();
                         case (7) -> SellerOptions.createMarket(scanner);
                         case (8) -> SellerOptions.deleteMarket();
                         case (9) -> running = false;
